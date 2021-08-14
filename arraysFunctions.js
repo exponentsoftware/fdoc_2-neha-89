81af53c3c4afd995532c49cc82cca3c4dd7da59b
Neha Sontakke <wasnik.neha@gmail.com>
2021-08-14 22:59:49 +0530

    solved 2.a

---
+++ arraysFunctions.js
@@ -0,0 +1,50 @@
+const users = [
+	{
+		name:'Brook', 
+		scores:75,
+		skills:['HTM', 'CSS', 'JS'],
+		age:16
+	},
+	{
+		name:'Alex', 
+		scores:80,
+		skills:['HTM', 'CSS', 'JS'],
+		age:18
+	}, 
+	{
+		name:'David', 
+		scores:75,
+		skills:['HTM', 'CSS'],
+		age:22
+	}, 
+	{
+		name:'John', 
+		scores:85,
+		skills:['HTM'],
+		age:25
+	},
+	{
+		name:'Sara',
+		scores:95,
+		skills:['HTM', 'CSS', 'JS'],
+		age: 26
+	},
+	{
+		name:'Martha', 
+		scores:80,
+		skills:['HTM', 'CSS', 'JS'],
+		age:18
+	},
+	{
+		name:'Thomas',
+		scores:90,
+		skills:['HTM', 'CSS', 'JS'],
+		age:20
+	}
+	];
+
+   const scoresGreaterThan85 = (users) => {
+       const scores85 = users.filter((item,index) => item.scores > 85);
+       console.log(scores85);
+   };
+   scoresGreaterThan85(users);
