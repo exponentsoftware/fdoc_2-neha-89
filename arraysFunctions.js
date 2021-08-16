035dc5d2e5dc776da68acd1b6349847a8c831956
Neha Sontakke <wasnik.neha@gmail.com>
2021-08-16 23:12:23 +0530

    solved 2c

--- arraysFunctions.js
+++ arraysFunctions.js
@@ -43,22 +43,44 @@ const users = [
 	}
 	];
 
-   const scoresGreaterThan85 = (users) => {
-       const scores85 = users.filter((item,index) => item.scores > 85);
-       console.log(scores85);
-   };
-   scoresGreaterThan85(users);
+//    const scoresGreaterThan85 = (users) => {
+//        const scores85 = users.filter((item,index) => item.scores > 85);
+//        console.log(scores85);
+//    };
+//    scoresGreaterThan85(users);
+// function addUser(user) {
+// 	var index = users.findIndex(x => x.name == user.name)
+// 	if (index === -1){
+// 		users.push(user);
+// 	}else {
+// 		console.log("user already exists")
+// 	}
+// }
+// var user = {name:"neha",scores:90,skills:['HTM', 'CSS', 'JS'],age:20};
+// addUser(user);
+// console.log(users);
 
 
-
-function addUser(user) {
-	var index = users.findIndex(x => x.name == user.name)
-	if (index === -1){
-		users.push(user);
-	}else {
-		console.log("user already exists")
+  const  addUserSkills =(user) => {
+	  const skill = [...users]
+	  const findSkill = skill.filter((item,index) => item.name ==user.name)
+	  if(findSkill.length == 0){
+		  return console.log("user doesn't exist");
+	  }
+	  findSkill[0].skills.push(...user.skills)
+	  console.log(findSkill[0].skills)
+	  console.log(addUserSkills);
+  }
+    newUserSkill = {
+	name: "Alex",
+	score: 80,
+	skills: ["MongoDB", "Node.Js", "Express"],
+	age: 25
 	}
-}
-var user = {name:"neha",scores:90,skills:['HTM', 'CSS', 'JS'],age:20};
-addUser(user);
-console.log(users);
+	addUserSkills(newUserSkill);
+    console.log(users);
+
+
+	
+
+	
